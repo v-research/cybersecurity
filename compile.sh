a7e115151e581c39f5c7f8cb8a6cb05e1e5436ae
v-research <marco@v-research.it>
2020-02-18 11:34:48 +0100

    bugfix

--- compile.sh
+++ compile.sh
@@ -9,18 +9,19 @@ fi
 
 if pdflatex -halt-on-error -output-directory=$latexOutputDir $latexInputFile
 then 
-	if [ "$1" != "-short" ] && [ "$1" != "-s" ] 
+	if [ "$1" != "-short" ] && [ "$1" != "-ss" ] 
 	then 
 		if biber --nodieonerror --input-directory=$latexOutputDir --output-directory=$latexOutputDir ${latexInputFile%.*}  
 		then
 			if pdflatex -halt-on-error -output-directory=$latexOutputDir $latexInputFile
 			then 
 				pdflatex -halt-on-error -output-directory=$latexOutputDir $latexInputFile 
-				if [[ -f $pdfOutputDir ]]
-				then
-					mv $pdfOutputDir `dirname $0`
-				fi
 			fi
 		fi
+	fi
+
+	if [[ -f $pdfOutputDir ]]
+	then
+		mv $pdfOutputDir `dirname $0`
 	fi
 fi
