5c1afc816c5e82f4ce7eb41ae461143968ac6971
v-research <marco@v-research.it>
2020-02-18 11:15:58 +0100

    add -short -s option to compile without biber

--- compile.sh
+++ compile.sh
@@ -9,14 +9,17 @@ fi
 
 if pdflatex -halt-on-error -output-directory=$latexOutputDir $latexInputFile
 then 
-	if biber --nodieonerror --input-directory=$latexOutputDir --output-directory=$latexOutputDir ${latexInputFile%.*}
-	then
-		if pdflatex -halt-on-error -output-directory=$latexOutputDir $latexInputFile
-		then 
-			pdflatex -halt-on-error -output-directory=$latexOutputDir $latexInputFile 
-			if [[ -f $pdfOutputDir ]]
-			then
-				mv $pdfOutputDir `dirname $0`
+	if [ $1 != "-short" ] && [ $1 != "-s" ]
+	then 
+		if [ biber --nodieonerror --input-directory=$latexOutputDir --output-directory=$latexOutputDir ${latexInputFile%.*} ] 
+		then
+			if pdflatex -halt-on-error -output-directory=$latexOutputDir $latexInputFile
+			then 
+				pdflatex -halt-on-error -output-directory=$latexOutputDir $latexInputFile 
+				if [[ -f $pdfOutputDir ]]
+				then
+					mv $pdfOutputDir `dirname $0`
+				fi
 			fi
 		fi
 	fi
