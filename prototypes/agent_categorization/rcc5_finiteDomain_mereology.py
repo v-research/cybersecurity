574a2bf7b6efd953998855b3e89260c0c594f681
v-research <marco@v-research.it>
2020-02-26 14:32:42 +0100

    parallel computation - first attempt

--- prototypes/agent_categorization/rcc5_finiteDomain_mereology.py
+++ prototypes/agent_categorization/rcc5_finiteDomain_mereology.py
@@ -1,6 +1,8 @@
 from z3 import *
 import scipy.special
 import itertools
+from multiprocessing import Process
+from multiprocessing import Queue 
 
 ################################
 # this method returns the rcc5 satisfiability table 
@@ -99,65 +101,65 @@ def rcc_five_sat_table(x,y,z,EQ,PP,PO,PPi,DR):
 
 ################################
 # since we unfold the quantifiers we have to calculate
-# the (minimal) number of subsets of A, B, and F that allows
+# the (minimal) number of subregions of A, B, and F that allows
 # Z3 to find a model that satisfies the agent configuration
 # 
-# For example, if we have A,B,F and no subsets of those 3 sets 
+# For example, if we have A,B,F and no subregions of those 3 regions 
 # there is no model that satisfies PO(A,B) /\ PO(B,F) /\ PO(A,F)
-# since PO(A,B) <-> PP(A,B) if A has no subsets 
+# since PO(A,B) <-> PP(A,B) if A has no subregions 
 #
 # requires scipy for binomial calculation: pip install scipy
 # otherwise download wheel from https://pypi.python.org/pypi/scipy
 # and install it with pip install <downloaded-file>
 ################################
-def calculate_sets(sets, Set):
-    set_of_subsets=[]
-    for k in range(1,len(sets)):
+def calculate_regions(regions, Region):
+    region_of_subregions=[]
+    for k in range(1,len(regions)):
         counter=0
-        for i in range(int(scipy.special.binom(len(sets), (k+1)))):
-            subset="subset_%s_%d"%((k+1), counter)
-            set_of_subsets.append(Const(subset, Set))
-            #print("%d - (%d %d)=%d"%(i,len(sets),(k+1),int(scipy.special.binom(len(sets), (k+1)))))
+        for i in range(int(scipy.special.binom(len(regions), (k+1)))):
+            subregion="subregion_%s_%d"%((k+1), counter)
+            region_of_subregions.append(Const(subregion, Region))
+            #print("%d - (%d %d)=%d"%(i,len(regions),(k+1),int(scipy.special.binom(len(regions), (k+1)))))
             counter+=1
         
-    return set_of_subsets
+    return region_of_subregions
 
-def topology(solver, sets_and_subsets, P):
+def topology(solver, regions_and_subregions, P):
     ################################
     # PART OF
     ################################
     
-    for s in sets_and_subsets:
+    for s in regions_and_subregions:
         #solver.add(P(s,s))
         solver.assert_and_track(P(s,s), str("riflexivity(%s)"%s))
     
-    for s1 in sets_and_subsets:
-        for s2 in sets_and_subsets:
+    for s1 in regions_and_subregions:
+        for s2 in regions_and_subregions:
             #solver.add(Implies(And(P(s1,s2),P(s2,s1)), s1==s2))
             solver.assert_and_track(Implies(And(P(s1,s2),P(s2,s1)), s1==s2), str("asymmetry(%s,%s)"%(s1,s2)))
     
-    for s1 in sets_and_subsets:
-        for s2 in sets_and_subsets:
-            for s3 in sets_and_subsets:
+    for s1 in regions_and_subregions:
+        for s2 in regions_and_subregions:
+            for s3 in regions_and_subregions:
                #solver.add(Implies(And(P(s1,s2),P(s2,s3)), P(s1,s3)))
                solver.assert_and_track(Implies(And(P(s1,s2),P(s2,s3)), P(s1,s3)), str("transitivity(%s,%s,%s)"%(s1,s2,s3)))
 
 ################################
 # with mereotopology we first define connects with and then part of
 ################################
-def mereotopology(solver, sets_and_subsets, C, P):
+def mereotopology(solver, regions_and_subregions, C, P):
     ################################
     # CONNECTS WITH
     # define here the relation C (primitive binary relation called connects with)  
     # which is reflexive and symmetric 
     ################################
     
-    for s in sets_and_subsets:
+    for s in regions_and_subregions:
         #solver.add(C(s,s))
         solver.assert_and_track(C(s,s), str("riflexivity(%s,%s)"%(s,s)))
     
-    for s1 in sets_and_subsets:
-        for s2 in sets_and_subsets:
+    for s1 in regions_and_subregions:
+        for s2 in regions_and_subregions:
             #solver.add(C(s1,s2)==C(s2,s1))
             solver.assert_and_track(C(s1,s2)==C(s2,s1), str("symmetry(%s,%s)"%(s1,s2)))
     
@@ -166,23 +168,23 @@ def mereotopology(solver, sets_and_subsets, C, P):
     # P(X,Y) : forall Z C(Z,X) => C(Z,Y)
     ################################
     
-    for s1 in sets_and_subsets:
-        for s2 in sets_and_subsets:
+    for s1 in regions_and_subregions:
+        for s2 in regions_and_subregions:
             array=[]
-            for s3 in sets_and_subsets:
+            for s3 in regions_and_subregions:
                 array.append(Implies(C(s3,s1), C(s3,s2)))
             #solver.add(P(s1,s2) == Implies(C(s3,s1), C(s3,s2))) 
             solver.assert_and_track(P(s1,s2) == And(array), str("P(%s,%s) and Z=%s"%(s1,s2,s3))) 
 
-def rcc_five(solver, sets_and_subsets, C, P, O, EQ, PP, PO, PPi, DR):
+def rcc_five(solver, regions_and_subregions, C, P, O, EQ, PP, PO, PPi, DR):
     ################################
     # OVERLAPS 
     # O(X,Y) : exists Z P(Z, X) /\ P(Z, Y) 
     ################################
-    for s1 in sets_and_subsets:
-        for s2 in sets_and_subsets:
+    for s1 in regions_and_subregions:
+        for s2 in regions_and_subregions:
             array=[]
-            for s3 in sets_and_subsets:
+            for s3 in regions_and_subregions:
                 array.append(And(P(s3,s1),P(s3,s2)))
             #solver.add(O(s1,s2) == Or(array)) 
             solver.assert_and_track(O(s1,s2) == Or(array), str("O(%s,%s) and Z=%s"%(s1,s2,s3))) 
@@ -191,8 +193,8 @@ def rcc_five(solver, sets_and_subsets, C, P, O, EQ, PP, PO, PPi, DR):
     # EQUAL TO 
     # EQ(X,Y) : P(X, Y ) /\ P(Y, X ) 
     ################################
-    for s1 in sets_and_subsets:
-        for s2 in sets_and_subsets:
+    for s1 in regions_and_subregions:
+        for s2 in regions_and_subregions:
             #solver.add(EQ(s1,s2) == And(P(s1,s2), P(s2,s1)))
             solver.assert_and_track(EQ(s1,s2) == And(P(s1,s2), P(s2,s1)), str("EQ(%s,%s)"%(s1,s2)))
     
@@ -200,8 +202,8 @@ def rcc_five(solver, sets_and_subsets, C, P, O, EQ, PP, PO, PPi, DR):
     # DISCRETE FROM
     # DR(X,Y) : not O(X,Y)
     ################################
-    for s1 in sets_and_subsets:
-        for s2 in sets_and_subsets:
+    for s1 in regions_and_subregions:
+        for s2 in regions_and_subregions:
             #solver.add(DR(s1,s2) == Not(O(s1,s2)))
             solver.assert_and_track(DR(s1,s2) == Not(O(s1,s2)), str("DR(%s,%s)"%(s1,s2)))
     
@@ -209,8 +211,8 @@ def rcc_five(solver, sets_and_subsets, C, P, O, EQ, PP, PO, PPi, DR):
     # PARTIAL OVERLAP
     # PO(X,Y) : O(X, Y) /\ (not P(X, Y)) /\ (not P(Y, X))
     ################################
-    for s1 in sets_and_subsets:
-        for s2 in sets_and_subsets:
+    for s1 in regions_and_subregions:
+        for s2 in regions_and_subregions:
             #solver.add(PO(s1,s2) == And(O(s1,s2), Not(P(s1,s2)), Not(P(s2,s1))))
             solver.assert_and_track(PO(s1,s2) == And(O(s1,s2), Not(P(s1,s2)), Not(P(s2,s1))), str("PO(%s,%s)"%(s1,s2)))
     
@@ -218,8 +220,8 @@ def rcc_five(solver, sets_and_subsets, C, P, O, EQ, PP, PO, PPi, DR):
     # PROPER PART OF 
     # PP(X,Y) : P(X, Y) /\ (not P(Y, X)) 
     ################################
-    for s1 in sets_and_subsets:
-        for s2 in sets_and_subsets:
+    for s1 in regions_and_subregions:
+        for s2 in regions_and_subregions:
             #solver.add(PP(s1,s2) == And(P(s1,s2), Not(P(s2,s1))))
             solver.assert_and_track(PP(s1,s2) == And(P(s1,s2), Not(P(s2,s1))), str("PP(%s,%s)"%(s1,s2)))
     
@@ -227,61 +229,73 @@ def rcc_five(solver, sets_and_subsets, C, P, O, EQ, PP, PO, PPi, DR):
     # INVERSE OF PROPER PART OF 
     # PPi(X,Y) : P(Y, X) /\ (not P(X, Y)) 
     ################################
-    for s1 in sets_and_subsets:
-        for s2 in sets_and_subsets:
+    for s1 in regions_and_subregions:
+        for s2 in regions_and_subregions:
             #solver.add(PPi(s1,s2) == PP(s2, s1)) #  And(P(s2,s1), Not(P(s1,s2))))
             solver.assert_and_track(PPi(s1,s2) == PP(s2, s1), str("PPi(%s,%s)"%(s1,s2))) #  And(P(s2,s1), Not(P(s1,s2))))
 
+def parallel_computation(solver, agent, queue):
+    p=Process(target=computation, args=(solver,agent,queue))
+    p.start()
+    p.join()
+    return p
+
+def computation(solver, agent, queue):
+    solver.push()
+    solver.add(agent)
+    queue.put(solver.check())
+    solver.pop()
+
 def main(mereo_topology="topology"):
     solver=Solver()
-    Set = DeclareSort('Set')
+    Region = DeclareSort('Region')
     
-    C  = Function('C', Set, Set, BoolSort())
-    P  = Function('P', Set, Set, BoolSort())
-    O  = Function('O', Set, Set, BoolSort())
-    EQ = Function('EQ', Set, Set, BoolSort())
-    DR = Function('DR', Set, Set, BoolSort())
-    PO = Function('PO', Set, Set, BoolSort())
-    PP = Function('PP', Set, Set, BoolSort())
-    PPi= Function('Pi', Set, Set, BoolSort())
+    C  = Function('C', Region, Region, BoolSort())
+    P  = Function('P', Region, Region, BoolSort())
+    O  = Function('O', Region, Region, BoolSort())
+    EQ = Function('EQ', Region, Region, BoolSort())
+    DR = Function('DR', Region, Region, BoolSort())
+    PO = Function('PO', Region, Region, BoolSort())
+    PP = Function('PP', Region, Region, BoolSort())
+    PPi= Function('Pi', Region, Region, BoolSort())
     
     ################################
     # SETS
     ################################
-    A = Const('A', Set)
-    B = Const('B', Set)
-    F = Const('F', Set)
+    A = Const('A', Region)
+    B = Const('B', Region)
+    F = Const('F', Region)
     
-    sets=[A,B,F]
+    regions=[A,B,F]
     
-    sets_and_subsets=sets + calculate_sets(sets, Set)
+    regions_and_subregions=regions + calculate_regions(regions, Region)
     
     # add partOf to solver
     if(mereo_topology=="mereotopology"):
         print("MEREOTOPOLOGY")
-        mereotopology(solver, sets_and_subsets, C, P)
+        mereotopology(solver, regions_and_subregions, C, P)
     elif(mereo_topology=="topology"):
         print("TOPOLOGY")
-        topology(solver, sets_and_subsets, P)
+        topology(solver, regions_and_subregions, P)
 
     # add rcc5 to the solver
-    rcc_five(solver, sets_and_subsets, C, P, O, EQ, PP, PO, PPi, DR)
+    rcc_five(solver, regions_and_subregions, C, P, O, EQ, PP, PO, PPi, DR)
     
     ################################
     # AGENT
     ################################
     
-    #from array of sets generates pairs of sets
-    pairs_sets=[[A, B], [B, F], [A, F]]
-    #pairs_sets=[]
-    #for i in range(len(sets)):
-    #    for j in range(i+1,len(sets)):
-    #        pairs_sets.append([sets[i],sets[j]])
+    #from array of regions generates pairs of regions
+    pairs_regions=[[A, B], [B, F], [A, F]]
+    #pairs_regions=[]
+    #for i in range(len(regions)):
+    #    for j in range(i+1,len(regions)):
+    #        pairs_regions.append([regions[i],regions[j]])
     
     #generates all the agents
     rcc5=[EQ,PP,PPi,PO,DR]
     itertables = []
-    for i in pairs_sets:
+    for i in pairs_regions:
         itertables.append(rcc5)
     
     counter=1
@@ -292,17 +306,21 @@ def main(mereo_topology="topology"):
     counter_correct_unsat=0
     k=1
     rcc5_sat_table=rcc_five_sat_table(A,B,F,EQ,PP,PO,PPi,DR)
+    processes=[]
+    queue=Queue()
     
     for t in itertools.product(*itertables):
         array_agent=[]
         for i in range(len(t)):
-            array_agent.append(t[i](pairs_sets[i]))
+            array_agent.append(t[i](pairs_regions[i]))
         agent=And(array_agent)
     
-        solver.push()
-        solver.add(agent)
-        check=solver.check()
+        #parallel computation B
+        processes.append(parallel_computation(solver, agent, queue))
+        #parallel computation E
     
+    for p in processes:
+        check=queue.get()
         correct=False
         rcc5_table_sat=False
         for row in rcc5_sat_table:
@@ -323,19 +341,17 @@ def main(mereo_topology="topology"):
             if(rcc5_table_sat):
                 counter_correct_sat+=1
                 correct=True
-        solver.pop()
     
-        offset1=' '*(5-len(str(counter)))
-        offset2=' '*(12-len(str(check)))
+        offregion1=' '*(5-len(str(counter)))
+        offregion2=' '*(12-len(str(check)))
         if(correct):
-            print("%d %s %s %s %s"%(counter, offset1, check, offset2, agent))
+            print("%d %s %s %s %s"%(counter, offregion1, check, offregion2, agent))
         else:
-            print("%d %s %s %s %s - %s"%(counter, offset1, check, offset2, agent, correct))
+            print("%d %s %s %s %s - %s"%(counter, offregion1, check, offregion2, agent, correct))
             if(check == sat):
                 print(solver.model())
             else:
                 print(solver.unsat_core())
-    
     
         counter+=1
     
