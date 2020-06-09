6424e7025ad09857dd7aca55e015a7816c7555c2
rocchettomarco <marco@v-research.it>
2020-06-09 18:29:39 +0200

    add compile script for conference paper

---
+++ reports/conference_paper/compile.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+latexOutputDir="latex-output-dir"
+latexInputFile="main.tex"
+pdfOutputDir="${latexOutputDir}/${latexInputFile%.*}.pdf"
+
+
+#if [[ ! -d $latexOutputDir ]]; then 
+rm -rf $latexOutputDir
+mkdir $latexOutputDir
+#fi
+
+if pdflatex -halt-on-error -output-directory=$latexOutputDir $latexInputFile
+then 
+	if [ "$1" != "-short" ] && [ "$1" != "-s" ] 
+	then 
+		if biber --nodieonerror --input-directory=$latexOutputDir --output-directory=$latexOutputDir ${latexInputFile%.*}  
+		then
+			if pdflatex -halt-on-error -output-directory=$latexOutputDir $latexInputFile
+			then 
+				pdflatex -halt-on-error -output-directory=$latexOutputDir $latexInputFile 
+			fi
+		fi
+	fi
+
+	if [[ -f $pdfOutputDir ]]
+	then
+		mv $pdfOutputDir `dirname $0`
+	fi
+fi
